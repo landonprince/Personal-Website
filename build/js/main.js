6e06a2fcdd9335315a3f810db30e493ff64c4920
landonprince <landoncprince@gmail.com>
2024-06-20 15:49:05 -0500

    v13

--- build/js/main.js
+++ build/js/main.js
@@ -12,3 +12,49 @@ const initApp = () => {
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
+
+const scrollers = document.querySelectorAll(".scroller");
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    scroller.setAttribute("data-animated", true);
+
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
+
+let currentlyVisibleElementId = null;
+
+function toggleVisibility(elementId, buttonId) {
+  var element = document.getElementById(elementId);
+  var button = document.getElementById(buttonId);
+
+  // Hide the currently visible element if it's different from the clicked one
+  if (currentlyVisibleElementId && currentlyVisibleElementId !== elementId) {
+    var currentElement = document.getElementById(currentlyVisibleElementId);
+    var currentButton = document.querySelector(
+      `button[onclick*="${currentlyVisibleElementId}"]`
+    );
+    currentElement.classList.add("hidden");
+    currentButton.textContent = "Show";
+  }
+
+  // Toggle the clicked element
+  if (element.classList.contains("hidden")) {
+    element.classList.remove("hidden");
+    button.textContent = "Hide";
+    currentlyVisibleElementId = elementId;
+  } else {
+    element.classList.add("hidden");
+    button.textContent = "Show";
+    currentlyVisibleElementId = null;
+  }
+}
